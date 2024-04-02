d11b6681c5cff88b5feb48500cb1bcc763bee12b
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-04-02 21:35:07 +0900

    feat: foreach文での出力

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,6 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<?php 
+$countries = ["ブラジル", "日本", "スペイン"]; 
+foreach ($countries as $number => $countryName) {
+    echo $number . "番目：" . $countryName . "が入っています。" . "\n";
+}
+?>
